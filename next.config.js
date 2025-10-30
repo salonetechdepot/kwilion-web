825cabbe551a762efc7a093a981cd1d30489425e
salonetechdepot <salonetechdepot@gmail.com>
2025-10-29 21:04:24 -0400

    Build: Tailwind/PostCSS v4 wiring, Next config tweak, deps tidy (lightningcss -> devDeps)

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  webpack: (config, { isServer }) => {
+    // Avoid noisy dynamic require warning from Sequelize
+    config.module.parser = config.module.parser || {};
+    config.module.parser.javascript = {
+      ...config.module.parser.javascript,
+      dynamicImportMode: "eager",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
